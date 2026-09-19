ca876443f0114b2d1fedb9a16d19f215ae767ea4
sandeepdolai <sandeepdolai.info@gmail.com>
2026-09-19 07:08:42 +0530

    Add Next.js App Router layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css'
+export const metadata={title:'Shakara',description:'Full-stack Shakara application'}
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
